0cb34addde47f0c358c820adb06abf26261c10f8
NeklinYT <nikitaekkemeev7911@gmail.com>
2024-06-16 23:32:33 +0400

    getcalculator edited

--- src/main/java/com/neklin/Main.java
+++ src/main/java/com/neklin/Main.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO
+        // TODO переместить getCalculator из Main в Calculator
         System.out.println("Calculator");
-        System.out.println("Write first number, second number, third number(if you like),  action");
-        System.out.println("example: 1   2  3 \nplus/minus/multiply/divide");
-        System.out.println("Do you like write three numbers? (y/n)");
+        System.out.println("Write first number, second number, third number(if you like)");
         int amount = 2;
         Calculator calculator = getCalculator();
         calculator.getResult();
     }
 
-    private static Calculator getCalculator() {
-        Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator();
-        char amountAnswer = scanner.next().charAt(0);
-        float firstNumber = scanner.nextFloat();
-        float secondNumber = scanner.nextFloat();
-        if (amountAnswer == 'y') {
-            float thirdNumber = scanner.nextFloat();
-            String action = scanner.next();
-            calculator.Calculate(firstNumber, secondNumber, thirdNumber, action);
-        } else {
-            String action = scanner.next();
-            calculator.Calculate(firstNumber, secondNumber, action);
-        }
-        return calculator;
+        private static Calculator getCalculator() {
+            Scanner scanner = new Scanner(System.in);
+            Calculator calculator = new Calculator();
+            String[] inputs = scanner.nextLine().split(" "); // ввод чисел
+            float[] numbers = new float[inputs.length]; // массив для хранения чисел
+            for (int i = 0; i < inputs.length; i++) {
+                numbers[i] = Float.parseFloat(inputs[i]); // парсинг строк в числа
+            }
+            System.out.println("example: plus/minus/multiply/divide");
+            System.out.println("Write action: ");
+            String action = scanner.next(); // ввод операции
+            if (numbers.length == 2) {
+                calculator.Calculate(numbers[0], numbers[1], action);
+            } else if (numbers.length == 3) {
+                calculator.Calculate(numbers[0], numbers[1], numbers[2], action);
+            } else {
+                System.out.println("Incorrect input");
+            }
+            return calculator;
     }
 }
